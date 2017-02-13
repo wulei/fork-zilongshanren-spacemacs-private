8d34761083a8030cd20c1a6821abc21198ee71ba
Leo <leowu56@gmail.com>
2017-02-13 08:38:20 +0800

    + csharp

--- init.el
+++ init.el
@@ -88,6 +88,7 @@ values."
               :variables chinese-enable-fcitx nil
               chinese-enable-youdao-dict t)
      leo
+     csharp
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
